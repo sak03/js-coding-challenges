feb8caeb6402470fd7ee05d82494f1a04e159d4e
sak03 <sartaj2394@gmail.com>
2025-02-05 22:10:48 +0530

    sorting array by method

--- array/sortArray.js
+++ array/sortArray.js
@@ -1,1 +1,14 @@
-console.log("Sorting array")
+/**
+ * Sort an array by both using inbuilt function and 
+ * using loops
+ */
+
+
+const arr = [1,1,2,3,4,5,6,7,6,5,4,0,9];
+
+// first method
+
+const sortedArr = arr.sort((a,b)=> a - b)
+
+
+console.log("Sorted Array: ", sortedArr)
