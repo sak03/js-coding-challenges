8df5f9cdc035318f2024f0e38ae6245a1604b13d
sak03 <sartaj2394@gmail.com>
2025-02-05 22:22:11 +0530

    sorting array by method

--- array/sortArray.js
+++ array/sortArray.js
@@ -4,11 +4,24 @@
  */
 
 
-const arr = [1,1,2,3,4,5,6,7,6,5,4,0,9];
+const arr = [1, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 0, 9];
 
 // first method
+const sortedArr = arr.sort((a, b) => a - b)
+console.log("Sorted Arr: ", sortedArr)
 
-const sortedArr = arr.sort((a,b)=> a - b)
+// second method
+const sortedArray = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
+}
 
-
-console.log("Sorted Array: ", sortedArr)
+console.log("Sorted Array: ", sortedArray(arr))
