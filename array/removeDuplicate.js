64ef32add23415af1019daf984817c570643b53b
sak03 <sartaj2394@gmail.com>
2025-02-22 12:19:02 +0530

    remove duplicates

--- array/removeDuplicate.js
+++ array/removeDuplicate.js
@@ -2,9 +2,26 @@
  * Remove duplicate elemnets from an array
  */
 
-const uniqueArr = (arr)=>{
-    const uniqueVal = [...new Set(arr)]
-    console.log("Result", uniqueVal)
-}
 
-uniqueArr([1,1,2,2,3,4,4,5,5,6,9])
+const arry = [1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 9]
+
+// method:1, by set method
+const uniqueArr1 = (arr)=>{
+    const uniqueVal = [...new Set(arr)]
+    return uniqueVal
+}
+const result1 = uniqueArr1(arry);
+// console.log("result1", result1)
+
+const uniqueArr2 = (arr)=>{
+    const newArr = []
+    for (let i = 0; i < arr.length; i ++){
+       if (!newArr.includes(arr[i])){
+           newArr.push(arr[i]); 
+       }
+   }
+    return newArr
+}
+const result2 = uniqueArr2(arry);
+console.log("result2", result2)
+
